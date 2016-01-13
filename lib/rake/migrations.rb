98b34ac38a0e28b650ca5959fb26aee67cefa24f
Michael Zaccari <michael.zaccari@accelerated.com>
2016-01-13 12:11:01 -0500

    Add basic logic to invoke rake tasks

--- lib/rake/migrations.rb
+++ lib/rake/migrations.rb
@@ -1,11 +1,22 @@
+require "rake"
 require "rake/migrations/version"
 require "rake/migrations/engine"
 
 module Rake
   module Migrations
 
-    def self.run_migrations
-      puts 'running migrations ...'
+    def self.run_task_migrations
+      get_task_migrations.each do |task|
+        invoke task
+      end
+    end
+
+    def self.get_task_migrations
+      ['app:version']
+    end
+
+    def self.invoke(task)
+      Rake::Task[task].invoke
     end
 
   end
--- lib/tasks/rake_migrations.rake
+++ lib/tasks/rake_migrations.rake
@@ -1,7 +1,7 @@
 namespace :tasks do
 
   task :migrate => :environment do
-    Rake::Migrations.run_migrations
+    Rake::Migrations.run_task_migrations
   end
 
 end
