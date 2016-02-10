165707b194e788e6e5fc8a0a8634c9a842ba7e57
Michael Zaccari <michael.zaccari@accelerated.com>
2016-02-10 17:02:12 -0500

    Add rspec config to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,27 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'rake/migrations'
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment', __FILE__)
+
+require 'rspec/rails'
+require 'database_cleaner'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before do |example|
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
