333cf3489f79596b577f407475ddf2ec16ac62be
Michael Zaccari <michael.zaccari@accelerated.com>
2016-02-10 13:40:55 -0500

    Add base tests

--- spec/rake/task_migration_spec.rb
+++ spec/rake/task_migration_spec.rb
@@ -1,11 +1,7 @@
 require 'spec_helper'
 
-describe Rake::Migrations do
+describe Rake::TaskMigration do
   it 'has a version number' do
     expect(Rake::Migrations::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
   end
 end
